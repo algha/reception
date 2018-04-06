6031466d56961794a0df763451758d60f0447df2
yusup <algha@outlook.com>
2018-04-06 20:05:55 +0900

    Update reply.js

--- app/module/messenger/util/reply.js
+++ app/module/messenger/util/reply.js
@@ -1,7 +1,7 @@
 request = require('request')
 
 var reply = {
-  token:"EAAeM9cnP6JUBAKE9atSYOnnipOP6IGde3rO9GySvZAWdLLZAHbGdXcURH2CbCUYZBybTnZCQ4xC9l9PtkIfUJuzfRcV4jPdnRxqvZC39zmRsISi75DKoJGvzrrixVabcu1PZA1DTIXuIXrsH5NRdhrHiVqFhAAWKs083sYr2TzFzVCa4LfT9ZBjWK6hhvIxSfgZD",
+  token:"",
   sendURL: "https://graph.facebook.com/v2.6/me/messages"
 }
 
